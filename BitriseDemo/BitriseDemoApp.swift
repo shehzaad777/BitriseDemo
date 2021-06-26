247649d7cf1530712b0398cbc882ae4eb5f023b1
shehzaad777 <shehzaad767@gmail.com>
2021-06-26 13:20:18 -0400

    sly bug

--- BitriseDemo/BitriseDemoApp.swift
+++ BitriseDemo/BitriseDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BitriseDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DemoView()
         }
     }
 }
--- BitriseDemo/ContentView.swift
+++ BitriseDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(\(undefined))
+        Text("Hello")
             .padding()
     }
 }
