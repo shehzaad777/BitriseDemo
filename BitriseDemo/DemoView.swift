2c06a4d3cad9c8e903038416d268e6f56fec2911
shehzaad777 <shehzaad767@gmail.com>
2021-06-26 13:31:23 -0400

    trial

---
+++ BitriseDemo/DemoView.swift
@@ -0,0 +1,20 @@
+//
+//  DemoView.swift
+//  BitriseDemo
+//
+//  Created by Shehzaad Daureeawoo on 6/26/21.
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoView()
+    }
+}
