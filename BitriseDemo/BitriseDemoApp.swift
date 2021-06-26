caf39fdb39930cfaacece987b8186a9ca33c7e06
shehzaad777 <shehzaad767@gmail.com>
2021-06-26 12:28:42 -0400

    Initial Commit

---
+++ BitriseDemo/BitriseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseDemoApp.swift
+//  BitriseDemo
+//
+//  Created by Shehzaad Daureeawoo on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BitriseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
